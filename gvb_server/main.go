4216243c18e0027c48edc956e1931106d5ad4eee
XIAOJIANG LIN <1227891082@qq.com>
2023-09-10 16:46:55 +0800

    项目搭建之日志配置

--- gvb_server/main.go
+++ gvb_server/main.go
@@ -8,6 +8,11 @@ import (
 func main() {
 	//读取配置文件
 	core.InitCore()
+	//初始化日志
+	global.Log = core.InitLogger()
+	global.Log.Warnln("hhhh")
+	global.Log.Error("hhhh")
+	global.Log.Infof("hhhh")
 	//	连接数据库
 	global.DB = core.InitGorm()
 }
