783400125a3d849f1f3c76ee84dc6acb8c1bfc8e
XIAOJIANG LIN <1227891082@qq.com>
2023-09-26 22:05:21 +0800

    顶部导航栏模块化

--- gvb_admin/src/views/admin/admin.vue
+++ gvb_admin/src/views/admin/admin.vue
@@ -19,33 +19,8 @@
 
 
           </div>
-          <div class="avatar">
-            <img src="http://s0yvn46ks.hn-bkt.clouddn.com/gvb/1695639074__6.jpg" alt="">
-          </div>
-          <div class="drop_menu">
-            <a-dropdown placement="bottomRight">
-              <a class="ant-dropdown-link" @click.prevent>
-                Janan
-                <i class="fa fa-angle-down"></i>
-              </a>
-              <template #overlay>
-                <a-menu @click="menuClick">
-                  <a-menu-item key="user_center">
-                    <a href="javascript:;">个人中心</a>
-                  </a-menu-item>
-                  <a-menu-item key="my_message">
-                    <a href="javascript:;">我的消息</a>
-                  </a-menu-item>
-                  <a-menu-item key="article_list">
-                    <a href="javascript:;">文章列表</a>
-                  </a-menu-item>
-                  <a-menu-item key="logout">
-                    <a href="javascript:;">注销退出</a>
-                  </a-menu-item>
-                </a-menu>
-              </template>
-            </a-dropdown>
-          </div>
+          <GVBUserInfo></GVBUserInfo>
+
         </div>
       </header>
       <div class="tabs"></div>
@@ -60,21 +35,7 @@ import {ref} from "vue";
 import GVBTheme from '../../components/gvb_theme.vue'
 import GVBAside from '../../components/admin/gvb_aside.vue'
 import GVBFullScreen from '../../components/gvb_full_screen.vue'
-
-const router = useRouter()
-const menuClick = ({key}) => {
-  if (key === "logout") {
-    console.log("logout")
-    return
-  }
-  router.push(
-      {
-        name: key,
-      },
-  )
-}
-
-
+import GVBUserInfo from '../../components/gvb_user_info.vue'
 
 
 </script>
@@ -118,17 +79,7 @@ const menuClick = ({key}) => {
         }
       }
 
-      .avatar {
-        img {
-          width: 40px;
-          height: 40px;
-          border-radius: 50%;
-        }
-      }
 
-      .drop_menu {
-        margin-left: 10px;
-      }
     }
 
     .tabs {
